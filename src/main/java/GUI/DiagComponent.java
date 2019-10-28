6a2302bc52341569f455fa487f6d001a98d0c0b6
VikaSannikova <vikasannikova1998@gmail.com>
2019-10-28 23:04:01 +0300

    fix double error bugs

--- src/main/java/GUI/DiagComponent.java
+++ src/main/java/GUI/DiagComponent.java
@@ -13,30 +13,36 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 
 public class DiagComponent extends JComponent {
-    JPanel dataInputPanel = new JPanel(new GridLayout(3, 2, 0, 5));
-    JPanel buttonPanel = new JPanel(new GridLayout(1, 1, 0, 5));
+    JPanel dataInputPanel = new JPanel(new GridLayout(0, 2, 0, 5));
+    JPanel buttonPanel = new JPanel(new GridLayout(2, 1, 0, 5));
     JPanel chartPanel = new JPanel();
     DefaultCategoryDataset dataset = new DefaultCategoryDataset();
     Timer timer;
 
     public DiagComponent() {
 
-        final JTextField[] textFields = new JTextField[3];
+        final ArrayList<JTextField> textFields = new ArrayList<JTextField>();
         for (int i = 0; i < 3; i++) {
-            textFields[i] = new JTextField();
-            textFields[i].setText("0");
+            JTextField tf = new JTextField();
+            tf.setText("0");
+            textFields.add(tf);
         }
         dataInputPanel.add(new JLabel("col1"));
-        dataInputPanel.add(textFields[0]);
+        dataInputPanel.add(textFields.get(0));
         dataInputPanel.add(new JLabel("col2"));
-        dataInputPanel.add(textFields[1]);
+        dataInputPanel.add(textFields.get(1));
         dataInputPanel.add(new JLabel("col3"));
-        dataInputPanel.add(textFields[2]);
+        dataInputPanel.add(textFields.get(2));
+        dataInputPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 
         JButton update = new JButton("Update");
+        JButton add = new JButton("New Column");
         buttonPanel.add(update);
+        buttonPanel.add(add);
+        buttonPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 
         dataset.addValue(0, "str", "0");
         dataset.addValue(0, "str", "1");
@@ -56,38 +62,68 @@ public class DiagComponent extends JComponent {
         NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
         rangeAxis.setRange(0, 10);
         chartPanel.add(new ChartPanel(chart));
-        final BigDecimal[] prevData = new BigDecimal[3];
-        final BigDecimal[] currData = new BigDecimal[3];
+        final ArrayList<BigDecimal> prevData = new ArrayList<BigDecimal>();
+        final ArrayList<BigDecimal> currData = new ArrayList<BigDecimal>();
+
         update.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                for (Integer i = 0; i < 3; i++) {
-                    prevData[i] = BigDecimal.valueOf(dataset.getValue("str", i.toString()).doubleValue());
-                    currData[i] = BigDecimal.valueOf(Double.parseDouble(textFields[i].getText()));
+                prevData.clear();
+                currData.clear();
+                for (Integer i = 0; i < dataInputPanel.getComponentCount()/2; i++) {
+                    prevData.add(BigDecimal.valueOf(dataset.getValue("str", i.toString()).doubleValue()));
+                    currData.add(BigDecimal.valueOf(Double.parseDouble(textFields.get(i).getText())));
                 }
                 timer.start();
             }
         });
 
+        add.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String s =Integer.toString(dataInputPanel.getComponentCount()/2);
+                String st = (Integer.toString(dataInputPanel.getComponentCount()/2 + 1));
+                dataInputPanel.add(new JLabel("col" + st));
+                JTextField tf = new JTextField();
+                tf.setText("0");
+                textFields.add(new JTextField("0"));
+                dataInputPanel.add(textFields.get(textFields.size()-1));
+                dataInputPanel.updateUI();
+                dataset.addValue(0, "str", s);
+                prevData.clear();
+                currData.clear();
+                for (Integer i = 0; i < dataInputPanel.getComponentCount()/2; i++) {
+                    prevData.add(BigDecimal.valueOf(dataset.getValue("str", i.toString()).doubleValue()));
+                    currData.add(BigDecimal.valueOf(Double.parseDouble(textFields.get(i).getText())));
+                }
+            }
+        });
+
+
         timer = new Timer(100, new ActionListener() {
-            BigDecimal delta = new BigDecimal(0.1);
+            BigDecimal delta = new BigDecimal("0.1");
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                for (Integer i = 0; i < 3; i++) {
+                for (Integer i = 0; i < dataInputPanel.getComponentCount()/2; i++) {
 
-                    if (prevData[i].compareTo(currData[i]) < 0){
-                        dataset.setValue(prevData[i].add(delta), "str", i.toString());
+                    if (prevData.get(i).compareTo(currData.get(i)) < 0){
+                        dataset.setValue((prevData.get(i)).add(delta), "str", i.toString());
                     }
-                    if(prevData[i].compareTo(currData[i])>0){
-                        dataset.setValue(prevData[i].add(delta.negate()), "str", i.toString());
+                    if(prevData.get(i).compareTo(currData.get(i))>0){
+                        dataset.setValue((prevData.get(i)).add(delta.negate()), "str", i.toString());
                     }
-                    prevData[i] = BigDecimal.valueOf(dataset.getValue("str", i.toString()).doubleValue());
+                    prevData.set(i, BigDecimal.valueOf(dataset.getValue("str", i.toString()).doubleValue()));
                     try {
                         Thread.sleep(1);
                     } catch (InterruptedException ex) {
                         ex.printStackTrace();
                     }
+                }
+                if(prevData.equals(currData)){
+                    prevData.clear();
+                    currData.clear();
+                    ((Timer)e.getSource()).stop();
                 }
             }
         });
